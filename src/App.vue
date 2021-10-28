13673ac46282803501264c23e5c3456b6996d9e0
yaoxin <271958091@qq.com>
2021-10-28 18:07:27 +0800

    路由

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,23 @@
 <template>
   <div id="app">
-    <button @click="toHome">首页</button>
-    <router-link v-bind:to="'/about/' + Id" tag="div">关于</router-link>
-    <button @click="toAbout">关于</button>
-    <router-link
-      v-bind:to="{path: '/profile', query: {name: '张三', age: 18}}">
-      我的
-    </router-link>
-    <router-view></router-view>
+    <tabbar></tabbar>
   </div>
 </template>
 <script>
+import Tabbar from './components/tabber/tabbar.vue'
 export default {
+  components: {
+    Tabbar
+  },
   name: 'App',
   data () {
     return {
-      Id: 35
     }
   },
   methods: {
-    toHome () {
-      this.$router.push({path: '/home', query: {type: 5}})
-    },
-    toAbout () {
-      this.$router.push('/about/' + this.Id)
-    }
   }
 }
 </script>
 <style scoped>
-.active {
-  color: #fff;
-}
+@import './assets/css/base.css';
 </style>
--- src/components/profile.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    This is my profile!
-    <h1>{{ $route.query.name }}</h1>
-    <h1>{{ $route.query.age }}</h1>
-  </div>
-</template>
