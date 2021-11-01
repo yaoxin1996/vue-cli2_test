1b524b2420cdcc12954df52c23ae4e868982b57e
yao <271958091@qq.com>
2021-11-01 21:14:02 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from '@/App'
 import router from '@/router'
 import store from '@/store'
+import axios from 'axios'
 Vue.config.productionTip = false
+
+// 123.207.32.32:8000/home/multidata
 
 /* eslint-disable no-new */
 new Vue({
@@ -10,4 +13,13 @@ new Vue({
   router,
   store,
   render: h => h(App)
+})
+
+axios.defaults.baseURL = 'http://123.207.32.32:8000'
+axios.defaults.timeout = 5000
+
+axios({
+  url: '/home/multidata'
+}).then(res => {
+  console.log(res)
 })
